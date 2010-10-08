376867b05768a647c3c8e6094896e27aa204a2b3
Kenny Gorman <kgorman@shutterfly.com>
2010-10-07 17:30:35 -0700

    fixes for issue#1 to allow for space to be summed by db per instance.

--- mongo_graph.py
+++ mongo_graph.py
@@ -1,7 +1,7 @@
 #
 # mongo_graph: a mongo -> rrd creation program
 # best used with something like drraw for display of the rrd's
-# v.3b
+# v.5b
 # 2010 Kenny Gorman
 #
 
@@ -23,12 +23,32 @@ class mongoGraph:
         port=port+1000  # mongodb always uses this port for stats
         names=[]
         values=[]
+        metrics={'avgObjSize':0,'dataSize':0,'storageSize':0,'indexSize':0,'fileSize':0}   # empty structure to hold aggregate stats
         print "%s: gathering %s stats" % (host,name)
-        if (name == "disk"): 
-            url="http://%s:%s/%s/$cmd/?filter_dbstats=1&limit=1" % (host,port,db)
-            data = json.loads(urllib.urlopen(url).read())
-            names =  data['rows'][0].keys()
-            values = data['rows'][0].values()
+        if (name == "disk"):
+            # disk stats are trickier, we have to traverse each db adding up the storage usage.
+            # get db list
+            url="http://%s:%s/admin/$cmd/?filter_listDatabases=1&limit=1" % (host,port)
+            try:
+               data = json.loads(urllib.urlopen(url).read())
+            except:
+                print("unable to read url %s") % url
+            # for each db, summate the disk usage
+            for i in data['rows'][0]['databases']:
+              url="http://%s:%s/%s/$cmd/?filter_dbstats=1&limit=1" % (host,port,i['name'])
+              print url
+              try:
+                datas = json.loads(urllib.urlopen(url).read())
+                c=0
+                for m in metrics.keys():
+                    metrics[m] = metrics[m]+datas['rows'][0][m]
+                    c+=1
+                metrics['avgObjSize'] = metrics['avgObjSize']/c  # this metric is a avg, not pure sum, so special handling
+              except:
+                print("no data found for url %s") % url
+            names = metrics.keys()
+            values = metrics.values()
+            
         if (name == "repl"):
             url="http://%s:%s/_status?repl=2" % (host,port)
             data = json.loads(urllib.urlopen(url).read())
